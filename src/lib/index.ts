014f97e6a41c73a5355eca3246fff40474ea5b31
Sambo Chea <7059827+sombochea@users.noreply.github.com>
2022-05-17 09:59:56 +0700

    Update index.ts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,5 @@
 export { default as Button } from "./button";
 export { default as Box } from "./box";
 export { default as Skeleton } from "./skeleton";
+export { default as Snackbar } from "./snackbar";
+export { default as Slide } from "./slide";
